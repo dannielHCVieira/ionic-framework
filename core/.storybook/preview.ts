f0f9bf9250fb3f8f4b8d1cc94d2a664dc7c8b5b9
Christian Bromann <git@bromann.dev>
2025-04-29 19:50:12 -0700

    enhance alert story

--- core/.storybook/preview.ts
+++ core/.storybook/preview.ts
@@ -1,12 +1,11 @@
-import { setMode } from '@stencil/core';
-import type { Preview } from '@stencil/storybook-plugin';
+import type { Preview } from '@stencil/storybook-plugin'
 
 /**
  * make sure all components are loaded
  */
 import { defineCustomElements } from '../loader/index.js'
 // @ts-expect-error no types for this file
-import { initialize } from '../components/index.js';
+import { initialize } from '../components/index.js'
 
 defineCustomElements()
 
@@ -24,17 +23,17 @@ export const globalTypes: Preview['globalTypes'] = {
       showName: true,
     },
   },
-};
+}
 
 const withMode = (Story, context) => {
   // Set the mode based on the global arg value
-  initialize({ mode: context.globals.mode });
+  initialize({ mode: context.globals.mode })
 
   // Return the story without any wrapper
-  return Story();
-};
+  return Story()
+}
 
-export const decorators: Preview['decorators'] = [withMode];
+export const decorators: Preview['decorators'] = [withMode]
 
 // Default parameters that apply to all stories
 export const parameters: Preview['parameters'] = {
@@ -45,4 +44,4 @@ export const parameters: Preview['parameters'] = {
       date: /Date$/,
     },
   }
-};
+}
--- core/src/components/alert/alert.stories.tsx
+++ core/src/components/alert/alert.stories.tsx
@@ -52,9 +52,6 @@ const meta: Meta<Alert> = {
     },
   },
   args: {
-    header: 'Alert',
-    message: 'This is an alert message.',
-    buttons: ['OK'],
     backdropDismiss: true,
     animated: true,
     isOpen: false,
@@ -65,11 +62,16 @@ export default meta;
 
 type Story = StoryObj<Alert>;
 
-export const Basic: Story = {
+/**
+ * Basic inline alert example as shown in the documentation
+ */
+export const InlineAlert: Story = {
   args: {
-    message: "Hello World!"
+    header: 'Alert',
+    subHeader: 'Subtitle',
+    message: 'This is an alert message.',
+    buttons: ['OK'],
   },
-
   render: (props) => (
     <div>
       <ion-alert {...props} trigger="open-alert"></ion-alert>
@@ -78,146 +80,305 @@ export const Basic: Story = {
   )
 };
 
-export const WithInputs: Story = {
+/**
+ * Example of using isOpen property to control the alert state
+ */
+export const UsingIsOpen: Story = {
   args: {
-    header: 'Login',
+    header: 'Alert',
+    message: 'This is an alert message controlled by isOpen.',
+    buttons: [
+      {
+        text: 'OK',
+        handler: () => {
+          console.log('Alert dismissed');
+          // Note: In a real application, you would set isOpen to false here
+        }
+      }
+    ],
+    isOpen: true
+  },
+  render: () => {
+    return (
+      <div>
+        <ion-button onClick={() => {
+          const alert = document.querySelector('ion-alert')!
+          alert.isOpen = true
+        }}>Click Me</ion-button>
+        <ion-alert
+          buttons={['Action']}
+          header="A Short Title Is Best"
+          sub-header="A Sub Header Is Optional"
+          message="A message should be a short, complete sentence."
+          overlayIndex={1000}
+          onIonAlertDidDismiss={function () {
+            console.log('Alert dismissed');
+            const alert = document.querySelector('ion-alert')!
+            alert.isOpen = false;
+          }}
+        ></ion-alert>
+      </div>
+    )
+  }
+};
+
+/**
+ * Example with multiple buttons, matching the documentation example
+ */
+export const ButtonsExample: Story = {
+  args: {
+    header: 'Alert',
+    message: 'This is an alert with multiple buttons and button handlers.',
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Open Modal',
+        handler: () => {
+          console.log('Open Modal clicked');
+        }
+      },
+      {
+        text: 'Delete',
+        role: 'destructive',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }
+    ]
+  },
+  render: (props) => (
+    <div>
+      <ion-alert {...props} trigger="open-buttons-alert"></ion-alert>
+      <ion-button id="open-buttons-alert">Show Alert with Buttons</ion-button>
+      <p>Check the console to see button handler messages.</p>
+    </div>
+  )
+};
+
+/**
+ * Example with text inputs, similar to the Text Inputs Example in documentation
+ */
+export const TextInputsExample: Story = {
+  args: {
+    header: 'Please enter your info',
+
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'OK',
+        handler: (data) => {
+          console.log('OK clicked, data:', data);
+        }
+      }
+    ],
+
     inputs: [
       {
-        name: 'username',
+        name: 'name',
         type: 'text',
-        placeholder: 'Username',
+        placeholder: 'Name',
       },
       {
-        name: 'password',
-        type: 'password',
-        placeholder: 'Password',
+        name: 'nickname',
+        type: 'text',
+        id: 'nickname-input',
+        placeholder: 'Nickname (max 8 characters)',
+        attributes: {
+          maxlength: 8,
+        },
       },
+      {
+        name: 'age',
+        type: 'number',
+        placeholder: 'Age',
+        min: 1,
+        max: 100,
+      },
+      {
+        name: 'about',
+        id: 'about-input',
+        type: 'textarea',
+        placeholder: 'A little about yourself',
+      }
     ],
-    buttons: [
-      {
-        text: 'Cancel',
-        role: 'cancel',
-      },
-      {
-        text: 'Login',
-      },
-    ],
+
+    translucent: true
   },
   render: (props) => (
     <div>
-      <ion-alert {...props} trigger="open-input-alert"></ion-alert>
-      <ion-button id="open-input-alert">Show Login Alert</ion-button>
+      <ion-alert {...props} trigger="open-text-inputs-alert"></ion-alert>
+      <ion-button id="open-text-inputs-alert">Show Text Inputs Alert</ion-button>
+      <p>Check the console to see form data on submit.</p>
     </div>
-  ),
+  )
 };
 
-export const WithRadioButtons: Story = {
+/**
+ * Example with radio buttons, matching the Radio Example in documentation
+ */
+export const RadioExample: Story = {
   args: {
-    header: 'Select an Option',
+    header: 'Radio',
     inputs: [
       {
-        name: 'option',
+        name: 'radio1',
         type: 'radio',
-        label: 'Option 1',
-        value: 'option1',
-        checked: true,
+        label: 'Radio 1',
+        value: 'value1',
+        checked: true
       },
       {
-        name: 'option',
+        name: 'radio2',
         type: 'radio',
-        label: 'Option 2',
-        value: 'option2',
+        label: 'Radio 2',
+        value: 'value2'
       },
       {
-        name: 'option',
+        name: 'radio3',
         type: 'radio',
-        label: 'Option 3',
-        value: 'option3',
+        label: 'Radio 3',
+        value: 'value3'
       },
+      {
+        name: 'radio4',
+        type: 'radio',
+        label: 'Radio 4',
+        value: 'value4'
+      },
+      {
+        name: 'radio5',
+        type: 'radio',
+        label: 'Radio 5',
+        value: 'value5'
+      },
+      {
+        name: 'radio6',
+        type: 'radio',
+        label: 'Radio 6 Radio 6 Radio 6',
+        value: 'value6'
+      }
     ],
-    buttons: [
-      {
-        text: 'Cancel',
-        role: 'cancel',
-      },
-      {
-        text: 'Confirm',
-      },
-    ],
-  },
-  render: (props) => (
-    <div>
-      <ion-alert {...props} trigger="open-radio-alert"></ion-alert>
-      <ion-button id="open-radio-alert">Show Radio Alert</ion-button>
-    </div>
-  ),
-};
-
-export const WithCheckboxes: Story = {
-  args: {
-    header: 'Select Options',
-    inputs: [
-      {
-        name: 'option1',
-        type: 'checkbox',
-        label: 'Option 1',
-        value: 'option1',
-        checked: true,
-      },
-      {
-        name: 'option2',
-        type: 'checkbox',
-        label: 'Option 2',
-        value: 'option2',
-      },
-      {
-        name: 'option3',
-        type: 'checkbox',
-        label: 'Option 3',
-        value: 'option3',
-      },
-    ],
-    buttons: [
-      {
-        text: 'Cancel',
-        role: 'cancel',
-      },
-      {
-        text: 'Confirm',
-      },
-    ],
-  },
-  render: (props) => (
-    <div>
-      <ion-alert {...props} trigger="open-checkbox-alert"></ion-alert>
-      <ion-button id="open-checkbox-alert">Show Checkbox Alert</ion-button>
-    </div>
-  ),
-};
-
-export const WithMultipleButtons: Story = {
-  args: {
-    header: 'Confirm Action',
-    message: 'Are you sure you want to proceed with this action?',
     buttons: [
       {
         text: 'Cancel',
         role: 'cancel',
         cssClass: 'secondary',
-      },
-      {
-        text: 'Delete',
-        role: 'destructive',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
       },
       {
         text: 'OK',
-      },
-    ],
+        handler: (data) => {
+          console.log('OK clicked, data:', data);
+        }
+      }
+    ]
   },
   render: (props) => (
     <div>
-      <ion-alert {...props} trigger="open-buttons-alert"></ion-alert>
-      <ion-button id="open-buttons-alert">Show Multi-Button Alert</ion-button>
+      <ion-alert {...props} trigger="open-radio-alert"></ion-alert>
+      <ion-button id="open-radio-alert">Show Radio Alert</ion-button>
+      <p>Check the console to see selected option on submit.</p>
     </div>
-  ),
+  )
+};
+
+/**
+ * Example with checkbox inputs
+ */
+export const CheckboxExample: Story = {
+  args: {
+    header: 'Checkbox',
+    inputs: [
+      {
+        name: 'checkbox1',
+        type: 'checkbox',
+        label: 'Checkbox 1',
+        value: 'value1',
+        checked: true
+      },
+      {
+        name: 'checkbox2',
+        type: 'checkbox',
+        label: 'Checkbox 2',
+        value: 'value2'
+      },
+      {
+        name: 'checkbox3',
+        type: 'checkbox',
+        label: 'Checkbox 3',
+        value: 'value3'
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'OK',
+        handler: (data) => {
+          console.log('OK clicked, data:', data);
+        }
+      }
+    ]
+  },
+  render: (props) => (
+    <div>
+      <ion-alert {...props} trigger="open-checkbox-alert"></ion-alert>
+      <ion-button id="open-checkbox-alert">Show Checkbox Alert</ion-button>
+      <p>Check the console to see selected options on submit.</p>
+    </div>
+  )
+};
+
+/**
+ * Example showing customization with CSS classes
+ */
+export const CustomizationExample: Story = {
+  args: {
+    header: 'Custom Alert',
+    subHeader: 'Customized using CSS classes',
+    message: 'This alert demonstrates customization capabilities using CSS custom properties.',
+    buttons: ['Dismiss'],
+    cssClass: 'my-custom-alert'
+  },
+  render: (props) => (
+    <div>
+      <style>{`
+        .my-custom-alert {
+          --background: #f8f8f8;
+          --max-width: 400px;
+        }
+
+        .my-custom-alert .alert-wrapper {
+          border-radius: 15px;
+        }
+
+        .my-custom-alert .alert-head {
+          color: #4d8cf4;
+        }
+      `}</style>
+      <ion-alert {...props} trigger="open-custom-alert"></ion-alert>
+      <ion-button id="open-custom-alert">Show Customized Alert</ion-button>
+      <p>This example shows how to customize an alert using CSS properties.</p>
+    </div>
+  )
 };
